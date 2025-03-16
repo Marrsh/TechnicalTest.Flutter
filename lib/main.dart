3eb88522387f6fa28bdc1e8907a795219d303986
Chris Marsh <chrism4rsh@gmail.com>
2025-03-16 22:12:13 +0000

    clean up unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tech_task/posts_bloc/posts_bloc.dart';
 import 'package:flutter_tech_task/router.dart';
-import 'package:flutter_tech_task/screens/post_details_screen.dart';
-import 'package:flutter_tech_task/screens/post_list_screen.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(MultiBlocProvider(
       providers: [BlocProvider(create: (context) => PostsBloc())],
-      child: MyApp()));
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
